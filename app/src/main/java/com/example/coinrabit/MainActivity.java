b867ee5fdb7dd5548e2ffab1aacd3f6a65ca6243
Shavipf <shavipf@gmail.com>
2021-05-24 21:37:58 -0500

    merge

--- app/src/main/java/com/example/coinrabit/MainActivity.java
+++ app/src/main/java/com/example/coinrabit/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tv_label = findViewById(R.id.lb_clickB);
+        //tv_label = findViewById(R.id.lb_clickB);
     }
 
     public void Click(View v){
--- app/src/main/java/com/example/coinrabit/chartActiviy.java
+++ app/src/main/java/com/example/coinrabit/chartActiviy.java
@@ -11,9 +11,6 @@ import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.components.LegendEntry;
 import com.github.mikephil.charting.components.XAxis;
-import com.github.mikephil.charting.components.YAxis;
-import com.github.mikephil.charting.data.BarData;
-import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.data.DataSet;
 import com.github.mikephil.charting.data.PieData;
@@ -27,9 +24,9 @@ import java.util.ArrayList;
 public class chartActiviy extends AppCompatActivity {
     private PieChart pieChart;
     private BarChart barChart;
-    private String[] months=new String[]{"Enero","Febrero","Marzo","Abril","Mayo",};
-    private int[] sale=new int[]{3200,1300,1500,2700,5000};
-    private int[] colors=new int[]{Color.BLACK,Color.RED,Color.GREEN,Color.BLUE, Color.LTGRAY};
+    private String[] months=new String[]{"Ingresos","Egresos"};
+    private int[] sale=new int[]{3200,1300};
+    private int[] colors=new int[]{Color.GREEN,Color.RED};
 
 
 
@@ -38,8 +35,8 @@ public class chartActiviy extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chart_activiy);
-        barChart=(BarChart)findViewById(R.id.barChart);
-        pieChart=(PieChart)findViewById(R.id.pieChart);
+        //barChart=(BarChart)findViewById(R.id.barChart);
+        pieChart=(PieChart)findViewById(R.id.pieChartLayout);
         createCharts();
     }
 
@@ -53,24 +50,23 @@ public class chartActiviy extends AppCompatActivity {
         //Se agrega el tiempo de animacion
         chart.getDescription().setTextColor(textColor);
         chart.animateY(animateTime);
-
         //retorna animacion
         legend(chart);
         return chart;
     }
 
     private void legend(Chart chart){
-      Legend legend=chart.getLegend();
-      legend.setForm(Legend.LegendForm.CIRCLE);
-      legend.setHorizontalAlignment(Legend.LegendHorizontalAlignment.CENTER);
-      ArrayList<LegendEntry>entries=new ArrayList<>();
-      for(int i=0;i< months.length;i++){
-          LegendEntry entry=new LegendEntry();
-          entry.formColor=colors[i];
-          entry.label=months[i];
-          entries.add(entry);
-      }
-      legend.setCustom(entries);
+        Legend legend=chart.getLegend();
+        legend.setForm(Legend.LegendForm.CIRCLE);
+        legend.setHorizontalAlignment(Legend.LegendHorizontalAlignment.CENTER);
+        ArrayList<LegendEntry>entries=new ArrayList<>();
+        for(int i=0;i< months.length;i++){
+            LegendEntry entry=new LegendEntry();
+            entry.formColor=colors[i];
+            entry.label=months[i];
+            entries.add(entry);
+        }
+        legend.setCustom(entries);
     }
     private ArrayList<BarEntry>getBarEntries(){
         ArrayList<BarEntry> entries= new ArrayList<>();
@@ -82,7 +78,8 @@ public class chartActiviy extends AppCompatActivity {
     private ArrayList<PieEntry>getPieEntries(){
         ArrayList<PieEntry> entries= new ArrayList<>();
         for(int i=0;i<sale.length;i++){
-            entries.add(new PieEntry(i,sale[i]));
+            entries.add(new PieEntry(sale[i],sale[i]));
+
         }
         return entries;
     }
@@ -93,48 +90,51 @@ public class chartActiviy extends AppCompatActivity {
         axis.setValueFormatter(new IndexAxisValueFormatter(months));
         axis.setEnabled(false);
     }
-    private void axisLeft(YAxis axis){
+    /*private void axisLeft(YAxis axis){
         axis.setSpaceTop(30);
         axis.setAxisMinimum(0);
     }
     private void axisRight(YAxis axis){
         axis.setEnabled(false);
-    }
+    }*/
+
     public void createCharts(){
-        barChart=(BarChart)getSameChart(barChart,"Ventas",Color.RED,Color.WHITE,3000);
-        barChart.setDrawGridBackground(true);
-        barChart.setDrawBarShadow(true);
-        barChart.setData(getBarData());
-        barChart.invalidate();
-        axisX(barChart.getXAxis());
-        axisLeft(barChart.getAxisLeft());
-        axisRight(barChart.getAxisRight());
+       //barChart=(BarChart)getSameChart(barChart,"Ventas",Color.RED,Color.WHITE,3000);
+        //barChart.setDrawGridBackground(true);
+        //barChart.setDrawBarShadow(true);
+        //barChart.setData(getBarData());
+        //barChart.invalidate();
+        //axisX(barChart.getXAxis());
+        //axisLeft(barChart.getAxisLeft());
+        //axisRight(barChart.getAxisRight());
         pieChart=(PieChart)getSameChart(pieChart,"Ventas",Color.GRAY,Color.WHITE,3000);
         pieChart.setHoleRadius(10);
         pieChart.setDrawHoleEnabled(false);
         pieChart.setTransparentCircleRadius(12);
         pieChart.setData(getPieData());
         pieChart.invalidate();
-
     }
+
     private DataSet getData(DataSet dataSet){
         dataSet.setColors(colors);
         dataSet.setValueTextColor(Color.WHITE);
-        dataSet.setValueTextSize(10);
+        dataSet.setValueTextSize(15);
         return dataSet;
     }
-    private BarData getBarData(){
+    /*private BarData getBarData(){
         BarDataSet barDataSet=(BarDataSet)getData(new BarDataSet(getBarEntries(),""));
         barDataSet.setBarShadowColor(Color.GRAY);
         BarData barData=new BarData(barDataSet);
         barData.setBarWidth(0.45f);
         return barData;
-    }
+    }*/
+
     private PieData getPieData(){
 
         PieDataSet pieDataSet=(PieDataSet)getData(new PieDataSet(getPieEntries(),""));
-        pieDataSet.setSliceSpace(2);
+        pieDataSet.setSliceSpace(0);
         pieDataSet.setValueFormatter(new PercentFormatter(pieChart));
-        return new PieData(pieDataSet);
+        PieData pd = new PieData(pieDataSet);
+        return pd;
     }
 }
